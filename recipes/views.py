66be07e5fc0eb4a7710753783479cf5f8791eb8c
Eduardo Lengler <eduardolengler@gmail.com>
2024-02-21 15:28:24 -0300

    Novas pastas e novos arquivos .html

--- recipes/views.py
+++ recipes/views.py
@@ -4,10 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Página HOME em views")
+    return render(request, 'recipes/home.html')
 
 def sobre(request):
-    return HttpResponse("Página SOBRE em views")
+    return render(request, "recipes/sobre.html")
 
 def contato(request):
-    return HttpResponse("Página CONTATO em views")
+    return render(request, "recipes/contato.html")
