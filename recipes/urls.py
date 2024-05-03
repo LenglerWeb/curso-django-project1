615a640456c8b82aef3d03e35c9c09c5ffcbd9a9
Eduardo Lengler <eduardolengler@gmail.com>
2024-05-03 15:30:44 -0300

    Conteúdo completo da aula 50 Sobre POO

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+# recipes:recipe
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home), #Home
-    path('recipes/<slug:id>/', views.recipe), #Página das notícias individuais
+    path('', views.home, name="home"), #Home
+    path('recipes/<slug:id>/', views.recipe, name="recipe"), #Página das notícias individuais
     
 ]
